8a6eca3172c20314425b7e20a6c127d6f18301af
Dustin <ducnt.vbi@vietinbank.vn>
2021-06-07 13:59:41 +0700

    first crud

---
+++ CRUDApp/Service/EmployeeService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDApp.Model;
+using CRUDApp.Context;
+namespace CRUDApp.Service
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public EmployeeContext _employeeDbContext;
+        public EmployeeService(EmployeeContext employeeDbContext)
+        {
+            _employeeDbContext = employeeDbContext;
+        }
+        public Employee AddEmployee(Employee e)
+        {
+            _employeeDbContext.Employees.Add(e);
+            _employeeDbContext.SaveChanges();
+            return e;
+        }
+        public List<Employee> GetEmployees()
+        {
+            return _employeeDbContext.Employees.ToList();
+        }
+        public void UpdateEmployee(Employee e)
+        {
+            _employeeDbContext.Employees.Update(e);
+            _employeeDbContext.SaveChanges();
+        }
+        public void DeleteEmployee(int Id)
+        {
+            var e = _employeeDbContext.Employees.FirstOrDefault(x => x.Id == Id);
+            if (e != null)
+            {
+                _employeeDbContext.Remove(e);
+                _employeeDbContext.SaveChanges();
+            }
+        }
+        //public Employee Get
+    }
+}
---
+++ CRUDApp/Service/IEmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDApp.Model;
+
+namespace CRUDApp.Service
+{
+    public interface IEmployeeService
+    {
+        Employee AddEmployee(Employee employee);
+        List<Employee> GetEmployee();
+        void UpdateEmployee(Employee employee);
+        void DeleteEmployee(int Id);
+        Employee GetEmployee(int Id);
+    }
+}
